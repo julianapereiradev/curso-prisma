00dbe2e1362f887da3ecab398eabc626e2e2a0c9
Juliana <julianapereira.dev@gmail.com>
2023-09-19 10:48:23 -0300

    Aula 3

---
+++ aula-prisma-03/src/repositories/post-repository.ts
@@ -0,0 +1,44 @@
+import { Post } from "@prisma/client";
+import prisma from "../database/database";
+
+const TABLE_NAME = "posts";
+
+export type CreatePost = Omit<Post, "id">
+
+async function getPosts() {
+const resultGet = await prisma.post.findMany()
+return resultGet
+}
+
+async function getPost(id: number) {
+const result = await prisma.post.findFirst({
+  where: {id}
+})
+return result
+}
+
+async function createPost(postInfo: CreatePost) {
+
+  const resultPost = await prisma.post.create({
+    data: postInfo
+
+  })
+  return resultPost
+}
+
+async function deletePost(id: number) {
+const resultDelete = await prisma.post.delete({
+  where: {id}
+})
+
+return resultDelete
+}
+
+const postRepository = {
+  getPost,
+  getPosts,
+  createPost,
+  deletePost
+}
+
+export default postRepository;
