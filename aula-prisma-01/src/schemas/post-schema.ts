b13ac9876bcefc014b5682b831677abee1e29168
Juliana <julianapereira.dev@gmail.com>
2023-09-19 08:51:59 -0300

    Aula 1

---
+++ aula-prisma-01/src/schemas/post-schema.ts
@@ -0,0 +1,13 @@
+import baseJoi, { Root } from 'joi';
+import joiDate from "@joi/date";
+
+const Joi: Root = baseJoi.extend(joiDate) 
+
+import { CreatePost } from "../repositories/post-repository";
+
+export const postSchema = Joi.object<CreatePost>({
+  username: Joi.string().required(),
+  title: Joi.string().required(),
+  content: Joi.string().required(),
+  createdAt: Joi.date().format('YYYY-MM-DD')
+})
